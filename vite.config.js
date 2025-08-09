2046d2a80d6e096e7f1a40cbc8ea5efe03f3bb87
Hijo1918 <jonathanije3@gmail.com>
2025-08-09 17:32:05 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['/assets/index-be91bb73.js']
-    }
-  }
+      external: ['react', 'react-dom', 'pages'],
+    },
+  },
 });
