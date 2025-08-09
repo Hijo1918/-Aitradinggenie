78aed7534e26ceb6d791666bba354e16b8dd3b5d
Hijo1918 <jonathanije3@gmail.com>
2025-08-09 17:02:22 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Navbar = ({ items }) => {
+const Navbar = ({ items = [] }) => {
   return (
-    <nav className="bg-blue-600 text-white p-4">
+    <nav className="bg-blue-600 text-white p-4" aria-label="Main Navigation">
       <ul className="flex space-x-6">
-        {items.map((item, index) => (
-          <li key={index}>
-            <a href={item.href} className="hover:text-gray-200">{item.label}</a>
+        {items.map((item) => (
+          <li key={item.href}>
+            <a href={item.href} className="hover:text-gray-200">
+              {item.label}
+            </a>
           </li>
         ))}
       </ul>
     </nav>
   );
+};
+
+Navbar.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      href: PropTypes.string.isRequired,
+    })
+  ),
 };
 
 export default Navbar;
